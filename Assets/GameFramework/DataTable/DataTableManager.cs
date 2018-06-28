8b34bf6dea862feb809cc63a6883a6e0baf63b04
DESKTOP-UBV38B7\codingworks <codingworks@outlook.com>
2018-06-28 16:19:30 +0800

    准备增加dataTable模块

---
+++ Assets/GameFramework/DataTable/DataTableManager.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #配置文件管理类# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2018年6月28日 16点03分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameFramework.Taurus
+{
+    public sealed class DataTableManager : GameFrameworkModule
+    {
+        //
+        //private Dictionary<string,I>
+
+        public void LoadDataTable<T>(string dataTable) where T :class, IDataTableRow,new()
+        {
+
+        }
+
+
+
+
+        public override void OnClose()
+        {
+        }
+    }
+}
---
+++ Assets/GameFramework/DataTable/IDataTable.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #配置文件管理类# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2018年6月28日 16点03分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameFramework.Taurus
+{
+
+    public interface IDataTable<T> where T : IDataTableRow
+    {
+
+    }
+}
