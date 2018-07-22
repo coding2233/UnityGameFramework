0a00d5c6c08880a687c8b4c68dface30c8ed4030
coding2233 <coding2233@163.com>
2018-07-23 00:36:43 +0800

    hotfix增加配置表

---
+++ HotFix/HotFix/GameFramework/DataTable/DataTableManager.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #配置文件管理类# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2018年6月28日 16点03分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HotFix.Taurus
+{
+    public sealed class DataTableManager : GameFrameworkModule
+    {
+
+		private readonly Dictionary<int, DataTableBase> _allDataTabvles;
+		private GameFramework.Taurus.ResourceManager _resource;
+
+	    public DataTableManager()
+	    {
+		    _allDataTabvles = new Dictionary<int, DataTableBase>();
+		    _resource = GameFramework.Taurus.GameFrameworkMode.GetModule<GameFramework.Taurus.ResourceManager>();
+	    }
+	    /// <summary>
+	    /// 加载数据表
+	    /// </summary>
+	    /// <typeparam name="T"></typeparam>
+	    /// <param name="data">配置表的数据</param>
+	    /// <returns></returns>
+		public void LoadDataTable<T>(string dataTablePath) where T :class, IDataTableRow,new()
+	    {
+		    string data= _resource.LoadAsset<TextAsset>(dataTablePath).text;
+		    DataTable<T> dataTable = new DataTable<T>();
+		    string[] rows = data.Split('\n');
+		    foreach (var item in rows)
+		    {
+				//排除多余的数据
+			    if (string.IsNullOrEmpty(item) || rows.Length == 0 || rows[0] == "#")
+				    continue;
+			    dataTable.AddDataRow(item);
+		    }
+		    int hasCode = typeof(T).GetHashCode();
+		    _allDataTabvles[hasCode] = dataTable;
+		}
+
+	    /// <summary>
+	    /// 获取数据表
+	    /// </summary>
+	    /// <typeparam name="T"></typeparam>
+	    /// <returns></returns>
+	    public IDataTable<T> GetDataTable<T>() where T : class, IDataTableRow, new()
+	    {
+		    DataTableBase dataTable = null;
+		    int hashCode = typeof(T).GetHashCode();
+		    if (_allDataTabvles.TryGetValue(hashCode, out dataTable))
+			    return (IDataTable<T>)dataTable;
+		    return null;
+	    }
+
+
+
+		public override void OnClose()
+        {
+	        _allDataTabvles.Clear();
+
+        }
+    }
+}
---
+++ HotFix/HotFix/GameFramework/DataTable/IDataTable.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #配置文件管理类# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2018年6月28日 16点03分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HotFix.Taurus
+{
+
+    public interface IDataTable<T> where T : class, IDataTableRow,new()
+    {
+		/// <summary>
+		/// 总数
+		/// </summary>
+	    int Count { get; }
+		/// <summary>
+		/// 获取数据行
+		/// </summary>
+		/// <param name="id">数据id</param>
+		/// <returns></returns>
+		T this[int id] { get; }
+		/// <summary>
+		/// 是否存在Id的数据行
+		/// </summary>
+		/// <param name="id">数据id</param>
+		/// <returns></returns>
+		bool HasDataRow(int id);
+	    /// <summary>
+	    /// 获取Id的数据行
+	    /// </summary>
+	    /// <param name="id"></param>
+	    /// <returns></returns>
+	    T GetDataRow(int id);
+	    /// <summary>
+	    /// 获取所有的数据行
+	    /// </summary>
+	    /// <returns></returns>
+	    T[] GetAllDataRows();
+	}
+}
