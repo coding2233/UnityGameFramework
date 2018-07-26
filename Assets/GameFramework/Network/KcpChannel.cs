f152cb5a78dd5d9493d3c7cd94bb82e31a11a7cc
coding2233 <coding2233@163.com>
2018-07-26 23:28:39 +0800

    添加kcp

---
+++ Assets/GameFramework/Network/KcpChannel.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #Kcp通道# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2018年7月26日 23点25分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameFramework.Taurus
+{
+	public class KcpChannel 
+	{
+
+	}
+
+}
