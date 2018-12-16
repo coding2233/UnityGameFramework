b8894fb4acbb2420a65664f30dc0c1796e124aee
wanderer <dutifulwanderer@gmail.com>
2018-12-16 14:19:07 +0800

    重写打包工具

--- Assets/GameFramework/Editor/AssetBundleEditor/AssetBundleBuildEditor.cs
+++ Assets/GameFramework/Editor/AssetBundleEditor/AssetBundleBuildEditor.cs
@@ -13,36 +13,59 @@ public class AssetBundleBuildEditor :EditorWindow {
 	private static string _outPutPath="";
 
 	private Vector2 _scrollViewPos;
+	private static string _rootPath;
 
-	[MenuItem("Tools/AssetBundles Options")]
+	private static readonly Dictionary<BuildTarget,bool> _allTargets=new Dictionary<BuildTarget, bool>();
+
+	[MenuItem("Tools/AssetBundles Options %#O")]
 	public static void AssetBundilesOptions()
 	{
+		_rootPath=Path.GetDirectoryName(Application.dataPath);
+
 		LoadConfig();
 
-		GetWindowWithRect<AssetBundleBuildEditor>(new Rect(Screen.width/2,Screen.height/2,400,30));
+		BuildTarget[] allTargets= (BuildTarget[])Enum.GetValues(typeof(BuildTarget));
+		//_allTargets
+		foreach (var item in allTargets)
+		{
+			int index=(int)item;
+			if(_config.BuildTargets.Contains(index))
+				_allTargets[item]=true;
+			else
+				_allTargets[item]=false;
+		}
+
+		GetWindowWithRect<AssetBundleBuildEditor>(new Rect(200,300,400,300),false,"Options");
 	}
 
-	[MenuItem("Tools/Build AssetBundles")]
+	[MenuItem("Tools/Build AssetBundles %#T")]
 	public static void BuildAssetBundles()
 	{
 		LoadConfig();
 
-		BuildPipeline.BuildAssetBundles(_config.BuildPath,BuildAssetBundleOptions.None,EditorUserBuildSettings.activeBuildTarget);
+		BuildTarget target=EditorUserBuildSettings.activeBuildTarget;
+		//打包编辑器的激活平台
+		BuildTarget(target);
+
+		//保存平台信息
+		SavePlatformVersion(new List<BuildTarget>(){target});
 	}
 
-	[MenuItem("Tools/Build AssetBundles All Targets")]
+	[MenuItem("Tools/Build AssetBundles Targets %#Y")]
 	public static void BuildAssetBundlesAllTargets()
 	{
 		LoadConfig();
 
-		//List<BuildTarget> targets=new List<BuildTarget>();
+		List<BuildTarget> targets=new List<BuildTarget>();
 		for(int i=0;i<_config.BuildTargets.Count;i++)
 		{
 			BuildTarget target= (BuildTarget)_config.BuildTargets[i];
-			string buildPath=Path.Combine(_config.BuildPath,target.ToString());
-			BuildPipeline.BuildAssetBundles(buildPath,BuildAssetBundleOptions.None,target);
+			BuildTarget(target);
+			targets.Add(target);
 		}
-		
+
+		//保存平台信息
+		SavePlatformVersion(targets);
 	}
 
 	/// <summary>
@@ -55,33 +78,52 @@ public class AssetBundleBuildEditor :EditorWindow {
 		{
 			return;
 		}
-
+		
 		GUILayout.BeginVertical("HelpBox");
 
 		GUILayout.BeginHorizontal("Box");
-		GUILayout.Label("Version");
+		GUILayout.Label("Version:");
 		GUILayout.Label(_config.Version.ToString());
 		if(GUILayout.Button("RESET",GUILayout.Width(60)))
 		{
-
+			_config.Version=0;
 		}
 		GUILayout.EndHorizontal();
 
 		GUILayout.BeginHorizontal("Box");
-		GUILayout.Label("BuildPath");
-		GUILayout.Label(_config.BuildPath);
+		GUILayout.Label("BuildPath:");
+		GUILayout.TextArea(string.IsNullOrEmpty(_config.BuildPath)?_rootPath:_config.BuildPath);
 
 		if(GUILayout.Button("BROWSE",GUILayout.Width(60)))
 		{
-
+			string path=(string.IsNullOrEmpty(_config.BuildPath)||!Directory.Exists(_config.BuildPath))?_rootPath:_config.BuildPath;
+		
+			path=EditorUtility.OpenFolderPanel("Build Path",path,"");
+			if(!string.IsNullOrEmpty(path))
+			{
+				if(path.Contains(_rootPath))
+				{
+					path=path.Replace(_rootPath,"");
+					if(path.IndexOf("/")==0)
+					{
+						path= path.Substring(1,path.Length-1);
+					}
+				}
+				_config.BuildPath=path;
+			}
+			return;
 		}
 		GUILayout.EndHorizontal();
 		
 		_scrollViewPos=GUILayout.BeginScrollView(_scrollViewPos,"Box");
-		string[] targets= Enum.GetNames(typeof(BuildTarget));
-		for(int i=0;i<targets.Length;i++)
+		foreach (var item in _allTargets)
 		{
-			GUILayout.Toggle(false,targets[i]);
+			bool value=GUILayout.Toggle(item.Value,item.Key.ToString());
+			if(value!=item.Value)
+			{
+				_allTargets[item.Key]=value;
+				break;
+			}
 		}
 		GUILayout.EndScrollView();
 
@@ -89,7 +131,10 @@ public class AssetBundleBuildEditor :EditorWindow {
 		GUILayout.FlexibleSpace();
 		if(GUILayout.Button("OK",GUILayout.Width(60)))
 		{
-
+			//保存配置文件
+			SaveConfig();
+			//关闭窗口
+			Close();
 		}
 		GUILayout.EndHorizontal();
 		
@@ -106,24 +151,111 @@ public class AssetBundleBuildEditor :EditorWindow {
 		}
 
 		_config=JsonUtility.FromJson<AssetBundleConifgInfo>(File.ReadAllText(_configPath));
-		// _config.Targets=new List<BuildTarget>();
-		// for(int i=0;i<_config.BuildTargets.Count;i++)
-		// {
-		// 	_config.Targets.Add((BuildTarget)_config.BuildTargets[i]);
-		// }
 	}
 
+	//保存配置信息
+	private static void SaveConfig()
+	{
+		_config.BuildTargets=new List<int>();
+		foreach(var item in _allTargets)
+		{
+			if (item.Value)
+			{
+				_config.BuildTargets.Add((int)item.Key);
+			}
+		}
+		string json=JsonUtility.ToJson(_config);
+		File.WriteAllText(_configPath,json);
+	}
 
+	//资源打包
+	private static void BuildTarget(BuildTarget target,BuildAssetBundleOptions options=BuildAssetBundleOptions.None)
+	{
+		string buildPath=Path.Combine(_config.BuildPath,target.ToString());
+		if(!Directory.Exists(buildPath))
+				Directory.CreateDirectory(buildPath);
+		BuildPipeline.BuildAssetBundles(buildPath,options,target);
 
+		//保存资源版本信息
+		SaveAssetVersion(buildPath,target);
+	}
+
+	//保存资源版本信息
+	private static void SaveAssetVersion(string buildPath,BuildTarget target)
+	{
+		AssetVersionInfo assetVersionInfo=new  AssetVersionInfo();
+		assetVersionInfo.Version=_config.Version;
+		assetVersionInfo.AssetHashInfos=new List<AssetHashInfo>();
+
+		AssetBundle targetBundle=AssetBundle.LoadFromFile(Path.Combine(buildPath,target.ToString()));
+		AssetBundleManifest manifest=targetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+		string[] assetNames=manifest.GetAllAssetBundles();
+		for (int i = 0; i < assetNames.Length; i++)
+		{
+			AssetHashInfo assetHashInfo=new AssetHashInfo();
+			assetHashInfo.Name=assetNames[i];
+			assetHashInfo.Hash=manifest.GetAssetBundleHash(assetNames[i]).ToString();
+			assetVersionInfo.AssetHashInfos.Add(assetHashInfo);
+		}
+
+		string json=JsonUtility.ToJson(assetVersionInfo);
+		File.WriteAllText(Path.Combine(buildPath,"version.txt"),json);
+		targetBundle.Unload(true);
+	}
+
+	//保存平台版本信息
+	private static void SavePlatformVersion(List<BuildTarget> targets)
+	{
+		PlatformVersionInfo platformInfo=new PlatformVersionInfo();
+		platformInfo.Version=_config.Version;
+		platformInfo.Platforms=new List<string>();
+		foreach (var item in targets)
+		{
+			platformInfo.Platforms.Add(item.ToString());
+		}
+		string json=JsonUtility.ToJson(platformInfo);
+		//保存平台信息
+		File.WriteAllText(Path.Combine(_config.BuildPath,"version.txt"),json);
+		//更新资源版本号 -- 保存配置文件
+		_config.Version++;
+		SaveConfig();
+		//打开文件夹
+		EditorUtility.OpenWithDefaultApp(_config.BuildPath);
+	}
 
 	//ab包的配置文件信息
 	[System.Serializable]
 	public class AssetBundleConifgInfo
 	{
-		public int Version=-1;
+		public int Version=0;
 		public string BuildPath="";
 		public List<int> BuildTargets=new List<int>();
 
 	}
-		
+
+	//平台资源信息
+	[System.Serializable]
+	public class PlatformVersionInfo
+	{
+		public int Version;
+		public List<string> Platforms=new List<string>();
+	}
+
+	//资源版本信息
+	[System.Serializable]
+	public class AssetVersionInfo
+	{
+		public int Version=0;
+		public List<AssetHashInfo> AssetHashInfos=new List<AssetHashInfo>();
+	}
+	
+	//资源hash值
+	[System.Serializable]
+	public class AssetHashInfo
+	{
+		public string Name;
+		public string Hash;
+	}
+
+	
 }
