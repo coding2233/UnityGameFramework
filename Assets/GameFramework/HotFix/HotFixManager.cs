5700f464810752edda7aacabb4c946ee5bb7827b
wanderer <dutifulwanderer@gmail.com>
2019-02-04 15:16:10 +0800

    添加xlua

---
+++ Assets/GameFramework/HotFix/HotFixManager.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//     Copyright (c) 2018 Zhang Yang. All rights reserved.
+// </copyright>
+// <describe> #热更新管理器# </describe>
+// <email> yeozhang@qq.com </email>
+// <time> #2019年2月4日 15点11分# </time>
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameFramework.Taurus
+{
+	public sealed class HotFixManager:GameFrameworkModule
+	{
+		
+
+		public override void OnClose()
+		{
+		}
+
+	}
+}
