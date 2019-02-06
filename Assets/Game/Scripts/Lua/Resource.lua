23c492ca1788bbe6bf854c23841dd7b8d87fe3ec
wanderer <dutifulwanderer@gmail.com>
2019-02-06 22:47:21 +0800

    添加lua resource的接口

--- Assets/Game/Scripts/Lua/Resource.lua
+++ Assets/Game/Scripts/Lua/Resource.lua
@@ -1,9 +1,26 @@
 Res={}
 
-local gameMode=CS.GameFramework.Taurus.GameMode
+local res=CS.GameFramework.Taurus.GameMode.Resource
 
 --@ 加载资源
-function Res:LoadAsset(target,ab,path)
-  target=gameMode.Resource:LoadAsset(target,ab,path)
+function Res:LoadAsset(target,abName,assetName)
+  target=res:LoadAsset(target,abName,assetName)
   return target
 end
+
+-- 卸载资源
+function Res:UnloadAsset(abName,unload)
+    res.UnloadAsset(abName,unload)
+end
+
+-- 加载场景
+function Res:LoadScene(abName,sceneName,mode)
+  return res.LoadSceneAsync(abName,sceneName,mode)
+end
+
+-- 卸载场景
+function Res:UnloadScene(sceneName)
+  res.UnloadSceneAsync(sceneName)
+end
+
+
