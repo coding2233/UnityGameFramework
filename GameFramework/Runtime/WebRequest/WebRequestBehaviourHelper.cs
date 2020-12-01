81ba14682ac436781f978614f49a5cdad792b172
wanderer <gitea@fake.local>
2020-12-01 18:09:59 +0800

    重构WebRequest

---
+++ GameFramework/Runtime/WebRequest/WebRequestBehaviourHelper.cs
@@ -0,0 +1,191 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Wanderer.GameFramework
+{
+    internal class WebRequestBehaviourHelper : MonoBehaviour
+    {
+
+        #region 接口
+        /// <summary>
+        /// 文本请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="header"></param>
+        /// <param name="callback"></param>
+        internal void RequestText(string url, Dictionary<string, string> header, Action<bool, string> callback)
+        {
+            StartCoroutine(WebRequestRequestText(url, header, callback));
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="header"></param>
+        /// <param name="body"></param>
+        /// <param name="callback"></param>
+        internal void RequestTextPost(string url, Dictionary<string, string> header, string body, Action<bool, string> callback)
+        {
+            StartCoroutine(WebRequestRequestTextPost(url, header, body, callback));
+        }
+
+        /// <summary>
+        /// 请求Texture2D
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="callback"></param>
+
+        internal void RequestTexture2D(string url, Action<Texture2D> callback)
+        {
+            StartCoroutine(WebRequestTexture2D(url, callback));
+        }
+
+        /// <summary>
+        /// 请求AssetBundle
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="callback"></param>
+        internal void RequestAssetBundle(string url, Action<AssetBundle> callback)
+        {
+            StartCoroutine(WebRequestAssetBundle(url, callback));
+        }
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="remoteUrl"></param>
+        /// <param name="localPath"></param>
+        /// <param name="callback"></param>
+        internal async Task Download(string remoteUrl, string localPath, Action<string, UnityWebRequest, float> callback)
+        {
+            await WebRequestDwonloaFile(remoteUrl,localPath, callback);
+        }
+        #endregion
+
+        #region 内部函数
+        //请求文本
+        private IEnumerator WebRequestRequestText(string url, Dictionary<string, string> header, Action<bool, string> callback)
+        {
+            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            {
+                if (header != null)
+                {
+                    foreach (var item in header)
+                    {
+                        request.SetRequestHeader(item.Key, item.Value);
+                    }
+                }
+                yield return request.SendWebRequest();
+                if (request.isNetworkError)
+                {
+                    callback?.Invoke(false, request.error);
+                }
+                else
+                {
+                    callback?.Invoke(true, request.downloadHandler.text);
+                }
+            }
+        }
+        //请求文本  POST
+        private IEnumerator WebRequestRequestTextPost(string url, Dictionary<string, string> header, string body, Action<bool, string> callback)
+        {
+            using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+            {
+                request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(body));
+                request.useHttpContinue = false;
+                if (header != null)
+                {
+                    foreach (var item in header)
+                    {
+                        request.SetRequestHeader(item.Key, item.Value);
+                    }
+                }
+                yield return request.SendWebRequest();
+                if (request.isNetworkError)
+                {
+                    callback?.Invoke(false, request.error);
+                }
+                else
+                {
+                    callback?.Invoke(true, request.downloadHandler.text);
+                }
+            }
+        }
+        //请求Texture2D
+        private IEnumerator WebRequestTexture2D(string url, Action<Texture2D> callback)
+        {
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
+            {
+                yield return request.SendWebRequest();
+                if (request.isNetworkError)
+                {
+                    callback?.Invoke(null);
+                }
+                else
+                {
+                    Texture2D tex = DownloadHandlerTexture.GetContent(request);
+                    callback?.Invoke(tex);
+                }
+            }
+        }
+
+        //请求AB包
+        private IEnumerator WebRequestAssetBundle(string url, Action<AssetBundle> callback)
+        {
+            using (UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url))
+            {
+                yield return request.SendWebRequest();
+                if (request.isNetworkError)
+                {
+                    callback?.Invoke(null);
+                }
+                else
+                {
+                    AssetBundle ab = DownloadHandlerAssetBundle.GetContent(request);
+                    callback?.Invoke(ab);
+                }
+            }
+        }
+
+        //下载文件
+        private IEnumerator WebRequestDwonloaFile(string remoteUrl, string localPath, Action<string, UnityWebRequest, float> callback)
+        {
+            //断点续传写不写呢...
+            //纠结------------------
+            //换一帧运行
+            yield return null;
+
+            using (UnityWebRequest request = UnityWebRequest.Get(remoteUrl))
+            {
+                request.downloadHandler = new DownloadHandlerFile(localPath);
+
+                request.timeout = 10;
+                float lastTime = Time.realtimeSinceStartup;
+                //yield return request.SendWebRequest(); 
+                request.SendWebRequest();
+
+
+                yield return null;
+                while (!request.isDone)
+                {
+                    float seconds = (Time.realtimeSinceStartup - lastTime);
+                    callback?.Invoke(localPath, request, lastTime);
+                    yield return null;
+                }
+
+                yield return null;
+
+                if (request.isNetworkError || request.isHttpError)
+                    callback?.Invoke(localPath, request, lastTime);
+                else
+                    callback?.Invoke(localPath, request, lastTime);
+            }
+
+        }
+		#endregion
+	}
+}
--- GameFramework/Runtime/WebRequest/WebRequestManager.cs
+++ GameFramework/Runtime/WebRequest/WebRequestManager.cs
@@ -20,14 +20,14 @@ namespace Wanderer.GameFramework
     public sealed class WebRequestManager : GameFrameworkModule
     {
         #region 属性
-
-        //事件管理类
-        private EventManager _event;
+        private WebRequestBehaviourHelper _webRequest;
         #endregion
 
         public WebRequestManager()
         {
-            _event = GameFrameworkMode.GetModule<EventManager>();
+            _webRequest = new GameObject("WebRequestBehaviourHelper").AddComponent<WebRequestBehaviourHelper>();
+            _webRequest.gameObject.hideFlags = HideFlags.HideAndDontSave;
+            GameObject.DontDestroyOnLoad(_webRequest.gameObject);
         }
         #region 外部接口
 
@@ -39,7 +39,7 @@ namespace Wanderer.GameFramework
         public Task<string> RequestText(string url, Dictionary<string, string> header)
         {
             var taskResult = new TaskCompletionSource<string>();
-            RequestText(url, header,(result, content) =>
+            _webRequest.RequestText(url, header,(result, content) =>
             {
                 taskResult.SetResult(result ? content : null);
             });
@@ -52,32 +52,9 @@ namespace Wanderer.GameFramework
         /// <param name="url"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public async void RequestText(string url, Dictionary<string, string> header, Action<bool, string> callback)
+        public void RequestText(string url, Dictionary<string, string> header, Action<bool, string> callback)
         {
-            try
-            {
-                UnityWebRequest request = UnityWebRequest.Get(url);
-                if (header != null)
-                {
-                    foreach (var item in header)
-                    {
-                        request.SetRequestHeader(item.Key, item.Value);
-                    }
-                }
-                await request.SendWebRequest();
-                if (request.isNetworkError)
-                {
-                    callback?.Invoke(false, request.error);
-                }
-                else
-                {
-                    callback?.Invoke(true, request.downloadHandler.text);
-                }
-            }
-            catch (System.Exception e)
-            {
-                callback?.Invoke(false, e.ToString());
-            }
+            _webRequest.RequestText(url, header, callback);
         }
 
         /// <summary>
@@ -87,36 +64,9 @@ namespace Wanderer.GameFramework
         /// <param name="header"></param>
         /// <param name="body"></param>
         /// <param name="callback"></param>
-        public async void RequestTextPost(string url,Dictionary<string,string> header,string body, Action<bool, string> callback)
+        public void RequestTextPost(string url,Dictionary<string,string> header,string body, Action<bool, string> callback)
         {
-            try
-            {
-                using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
-                {
-                    request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(body));
-                    request.useHttpContinue = false;
-                    if (header != null)
-                    {
-                        foreach (var item in header)
-                        {
-                            request.SetRequestHeader(item.Key, item.Value);
-                        }
-                    }
-                    await request.SendWebRequest();
-                    if (request.isNetworkError)
-                    {
-                        callback?.Invoke(false, request.error);
-                    }
-                    else
-                    {
-                        callback?.Invoke(true, request.downloadHandler.text);
-                    }
-                }
-            }
-            catch (System.Exception e)
-            {
-                callback?.Invoke(false, e.ToString());
-            }
+            _webRequest.RequestTextPost(url, header, body, callback);
         }
 
         /// <summary>
@@ -127,7 +77,7 @@ namespace Wanderer.GameFramework
         public Task<Texture2D> RequestTexture2D(string url)
         {
             var resultTask = new TaskCompletionSource<Texture2D>();
-            RequestTexture2D(url, (result, tex2d) =>
+            _webRequest.RequestTexture2D(url, (tex2d) =>
             {
                 resultTask.SetResult(tex2d);
             });
@@ -140,26 +90,9 @@ namespace Wanderer.GameFramework
         /// <param name="url"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public async void RequestTexture2D(string url, Action<bool, Texture2D> callback)
+        public void RequestTexture2D(string url, Action<Texture2D> callback)
         {
-            try
-            {
-                UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-                await request.SendWebRequest();
-                if (request.isNetworkError)
-                {
-                    callback?.Invoke(false, null);
-                }
-                else
-                {
-                    Texture2D tex = DownloadHandlerTexture.GetContent(request);
-                    callback?.Invoke(true, tex);
-                }
-            }
-            catch (System.Exception e)
-            {
-                callback?.Invoke(false, null);
-            }
+            _webRequest.RequestTexture2D(url, callback);
         }
 
         /// <summary>
@@ -170,7 +103,7 @@ namespace Wanderer.GameFramework
         public Task<AssetBundle> RequestAssetBundle(string url)
         {
             var resultTask = new TaskCompletionSource<AssetBundle>();
-            RequestAssetBundle(url, (result, ab) =>
+            _webRequest.RequestAssetBundle(url, (ab) =>
             {
                 resultTask.SetResult(ab);
             });
@@ -183,27 +116,9 @@ namespace Wanderer.GameFramework
         /// <param name="url"></param>
         /// <param name="callback"></param>
         /// <returns></returns>
-        public async void RequestAssetBundle(string url, Action<bool, AssetBundle> callback)
+        public void RequestAssetBundle(string url, Action<AssetBundle> callback)
         {
-            try
-            {
-                UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
-                await request.SendWebRequest();
-                if (request.isNetworkError)
-                {
-                    callback?.Invoke(false, null);
-                }
-                else
-                {
-                    AssetBundle ab = DownloadHandlerAssetBundle.GetContent(request);
-                    callback?.Invoke(true, ab);
-                }
-            }
-            catch (System.Exception e)
-            {
-                callback?.Invoke(false, null);
-            }
-
+            _webRequest.RequestAssetBundle(url, callback);
         }
 
         /// <summary>
@@ -213,57 +128,18 @@ namespace Wanderer.GameFramework
         /// <param name="localPath"></param>
         /// <param name="resultCallback"></param>
         /// <param name="progressCallback"></param>
-        public async Task Download(string remoteUrl, string localPath, Action<string, UnityWebRequest, float> callback)
+        public Task Download(string remoteUrl, string localPath, Action<string, UnityWebRequest, float> callback)
         {
-            await UnityWebRequestDwonloaFile(remoteUrl, localPath, callback);
+            return _webRequest.Download(remoteUrl,localPath, callback);
         }
-
         #endregion
 
         public override void OnClose()
         {
-
+            GameObject.Destroy(_webRequest.gameObject);
+            _webRequest = null;
         }
 
-        #region 内部函数
-
-
-        //下载文件
-        IEnumerator UnityWebRequestDwonloaFile(string remoteUrl, string localPath, Action<string, UnityWebRequest,float> callback)
-        {
-            
-            //断点续传写不写呢...
-            //纠结------------------
-
-            //换一帧运行
-            yield return null;
-           
-            UnityWebRequest request = UnityWebRequest.Get(remoteUrl);
-			request.downloadHandler = new DownloadHandlerFile(localPath);
-            
-            request.timeout = 10;
-            float lastTime = Time.realtimeSinceStartup;
-            //yield return request.SendWebRequest(); 
-            request.SendWebRequest();
-            
-            
-            yield return null;
-            while (!request.isDone)
-            {
-                float seconds = (Time.realtimeSinceStartup - lastTime);
-                callback?.Invoke(localPath, request, lastTime);
-                yield return null;
-            }
-
-            yield return null;
-
-            if (request.isNetworkError || request.isHttpError)
-                callback?.Invoke(localPath, request, lastTime);
-            else
-                callback?.Invoke(localPath, request, lastTime);
-        }
-
-        #endregion
 
     }
 }
