a8bef93e5b679b0e9447d30c75e26e05e12542c2
wanderer <gitea@fake.local>
2020-12-02 10:54:43 +0800

    增加资源管理的Sprite的扩展

---
+++ GameFramework/Runtime/Resource/ResourceManagerExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wanderer.GameFramework
+{
+	public static class ResourceManagerExtension
+	{
+		public static void LoadSprite(this ResourceManager resource, string assetPath, Action<Sprite> callback)
+		{
+
+			resource.LoadAsset<Sprite>(assetPath, callback);
+		}
+
+		public static Sprite LoadSpriteSync(this ResourceManager resource, string assetPath)
+		{
+			return resource.LoadAssetSync<Sprite>(assetPath);
+		}
+	}
+}
