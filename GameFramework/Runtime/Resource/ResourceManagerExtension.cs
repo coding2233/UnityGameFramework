5b149c24aa921ecdefb5193db70d92a060c203be
wanderer <gitea@fake.local>
2020-12-10 10:19:16 +0800

    增加Resource加载Object的接口

--- GameFramework/Runtime/Resource/ResourceManagerExtension.cs
+++ GameFramework/Runtime/Resource/ResourceManagerExtension.cs
@@ -7,6 +7,18 @@ namespace Wanderer.GameFramework
 {
 	public static class ResourceManagerExtension
 	{
+		//Object
+		public static void LoadObject(this ResourceManager resource, string assetPath, Action<UnityEngine.Object> callback)
+		{
+			resource.Asset.LoadAsset<UnityEngine.Object>(assetPath, callback);
+		}
+
+		public static UnityEngine.Object LoadObjectSync(this ResourceManager resource, string assetPath)
+		{
+			return resource.Asset.LoadAsset<UnityEngine.Object>(assetPath);
+		}
+
+		//GameObject
 		public static void LoadGameObject(this ResourceManager resource, string assetPath, Action<GameObject> callback)
 		{
 			resource.Asset.LoadAsset<GameObject>(assetPath, callback);
@@ -17,6 +29,7 @@ namespace Wanderer.GameFramework
 			return resource.Asset.LoadAsset<GameObject>(assetPath);
 		}
 
+		//Texture
 		public static void LoadTextAsset(this ResourceManager resource, string assetPath, Action<TextAsset> callback)
 		{
 			resource.Asset.LoadAsset<TextAsset>(assetPath, callback);
@@ -26,7 +39,8 @@ namespace Wanderer.GameFramework
 		{
 			return resource.Asset.LoadAsset<TextAsset>(assetPath);
 		}
-
+		
+		//Sprite
 		public static void LoadSprite(this ResourceManager resource, string assetPath, Action<Sprite> callback)
 		{
 			resource.Asset.LoadAsset<Sprite>(assetPath, callback);
